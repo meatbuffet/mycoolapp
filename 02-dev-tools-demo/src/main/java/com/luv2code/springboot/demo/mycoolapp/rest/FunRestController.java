82bd87b2b6a598443e0b10dbd2891a0ac84efbbf
meatbuffet <mailabyss.1@gmail.com>
2022-03-31 21:34:39 +0500

    spring boot 2

---
+++ 02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+//71.2
+@RestController
+public class FunRestController {
+
+    //expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on the server is " + LocalDateTime.now();
+    }
+
+//     expose a new endpoing for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day";
+    }
+}
